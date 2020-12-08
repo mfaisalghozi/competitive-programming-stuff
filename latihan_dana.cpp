e30ee47a2d9c73e4c99f874c2b6cb0fb793eba16
mfaisalghozi <mrafgaming@gmail.com>
2020-12-08 13:13:04 +0700

    latihan dana interview

--- latihan_dana.cpp
+++ latihan_dana.cpp
@@ -4,6 +4,15 @@
 #include <string.h>
 
 int main(){
-	printf("hello world");
+	int n, t , arr[t+1], arr2[];
+	scanf("%d", &n);
+	for(int j=0;j<n;j++){
+		scanf("%d", &t);
+		for(int k=0;k<t;k++){
+			scanf("%d",arr[k]);
+		}		
+	}
+	
+	
 	return 0;
 }
